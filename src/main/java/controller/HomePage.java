bcf5fae398615a0efa60568cec59d64642270702
Your Name <you@example.com>
2023-02-18 17:15:03 +0700

    duc commit

--- src/main/java/controller/HomePage.java
+++ src/main/java/controller/HomePage.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
@@ -57,41 +56,62 @@ public class HomePage implements Initializable {
         }
         homePageButton.focusedProperty();
     }
+    public void removeStyleButton() throws IOException{
+        homePageButton.setStyle("-fx-background-color: #fed6e3");
+        exportInvoiceButton.setStyle("-fx-background-color: #fed6e3");
+        storageButton.setStyle("-fx-background-color: #fed6e3");
+        managementButton.setStyle("-fx-background-color: #fed6e3");
+        customerButton.setStyle("-fx-background-color: #fed6e3");
+        repairButton.setStyle("-fx-background-color: #fed6e3");
+
+    }
     public void storageButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane storagePane =  FXMLLoader.load(main.class.getResource("/view/Kho/StoragePane.fxml"));
         mainPane.getChildren().add(storagePane);
         trangChuLabel.setStyle("");
+        removeStyleButton();
+        storageButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void homePageButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane trangChuPane =  FXMLLoader.load(main.class.getResource("TrangChuPane.fxml"));
         mainPane.getChildren().add(trangChuPane);
+        removeStyleButton();
+        homePageButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void exportInvoiceButtonButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane exportInvoicePane =  FXMLLoader.load(main.class.getResource("/view/PhieuXuat/ExportInvoicePane.fxml"));
         mainPane.getChildren().add(exportInvoicePane);
+        removeStyleButton();
+        exportInvoiceButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void repairButtonButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane BHBTPane =  FXMLLoader.load(main.class.getResource("/view/BaoHanh/BaoHanhPane.fxml"));
         mainPane.getChildren().add(BHBTPane);
+        removeStyleButton();
+        repairButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void managementButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane trangChuPane =  FXMLLoader.load(main.class.getResource("/view/QuanLy/quanlypage.fxml"));
         mainPane.getChildren().add(trangChuPane);
+        removeStyleButton();
+        managementButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void customerButtonOnAction(ActionEvent event) throws IOException{
         mainPane.getChildren().clear();
         Pane trangChuPane =  FXMLLoader.load(main.class.getResource("/view/KhachHang/CustomerPane.fxml"));
         mainPane.getChildren().add(trangChuPane);
+        removeStyleButton();
+        customerButton.setStyle("-fx-background-color: linear-gradient(to top, #ffccff 0%, #fed6e3 100%)");
     }
 
     public void dangXuatButtonOnAction(ActionEvent event) throws IOException {
